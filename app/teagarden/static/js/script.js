f68ca9ddb6dbd082fe98056a413f82d0ef12d94f
Henning Kage <henning.kage@googlemail.com>
2011-04-12 15:26:34 +0200

    Prossible fix for JS URL composition

--- app/teagarden/static/js/script.js
+++ app/teagarden/static/js/script.js
@@ -19,7 +19,7 @@ teagarden.show_popup = function(obj, url_part, url_mapper) {
   var index = url.indexOf(url_part);
   var key = url.substring(index + url_part.length + 1);
   var xhr = new goog.net.XhrIo();
-  goog.net.XhrIo.send('/' + base_url + url_mapper + key, function(e) {
+  goog.net.XhrIo.send(base_url + url_mapper + key, function(e) {
     var xhr = e.target;
     var response = xhr.getResponseText();
     var tooltip = new goog.ui.Tooltip(obj);
@@ -38,7 +38,7 @@ teagarden.show_popup = function(obj, url_part, url_mapper) {
   */
 teagarden.set_object_star = function(id, url, name) {
   var xhr = new goog.net.XhrIo();
-  goog.net.XhrIo.send('/' + base_url + id + url, function(e) {
+  goog.net.XhrIo.send(base_url + id + url, function(e) {
     var xhr = e.target;
     var response = xhr.getResponseText();
     var el = goog.dom.getElement(name + '-star-' + id);
@@ -98,7 +98,7 @@ teagarden.shortcuts.handle = function(event) {
       teagarden.shortcuts.show_help();
       break;
     case "GD":
-      document.location.href = '/dashboard';
+      document.location.href = dashboard_link;
       break;
     case "M":
       document.location.href = publish_link;
