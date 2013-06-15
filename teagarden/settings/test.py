9664f00456e04db66f1f50f53e72ced89cfa2a18
Henning Kage <henning.kage@gmail.com>
2013-06-15 16:54:52 +0200

    Changed settings file

--- teagarden/settings/test.py
+++ teagarden/settings/test.py
@@ -23,22 +23,3 @@ CACHES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-    'SHOW_TOOLBAR_CALLBACK': lambda r: not r.path.startswith('/admin/'),
-}
-
-INTERNAL_IPS = ('127.0.0.1')
-
-TEMPLATE_CONTEXT_PROCESSORS += (
-    'django.core.context_processors.debug',
-)
